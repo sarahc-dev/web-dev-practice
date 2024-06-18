1ddce760f850bd9310159fb54755df5df1638447
Sarah Clements <sarahclements@hotmail.co.uk>
2024-06-18 13:36:14 +0100

    add part 3 - add path variable for language

--- src/main/kotlin/org/example/Server.kt
+++ src/main/kotlin/org/example/Server.kt
@@ -6,6 +6,7 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
+import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
@@ -15,7 +16,24 @@ val app: HttpHandler = routes(
         request: Request ->
         val name = request.query("name")?.let { " $it" } ?: ""
         Response(OK).body("Hello$name")
+    },
+    "/{language}/hello" bind Method.GET to {
+        request: Request ->
+        var name = request.query("name")?.let { " $it" } ?: ""
+        val greeting = when (request.path("language")) {
+            "fr-FR" -> "Bonjour"
+            "en-AU" -> "G'day"
+            "it-IT" -> "Salve"
+            "en-GB" -> {
+                if (name != "") name += "?"
+                if (name == "") "Alright" else "Alright,"
+            }
+            else -> "Hello"
+        }
+
+        Response(OK).body("$greeting$name")
     }
+
 )
 
 fun main() {
--- src/test/kotlin/org/example/ServerTest.kt
+++ src/test/kotlin/org/example/ServerTest.kt
@@ -17,4 +17,22 @@ class ServerTest {
     fun `if user visits hello endpoint and includes name param it responds Hello and name`() {
         assertEquals(Response(OK).body("Hello Sarah"), app(Request(Method.GET, "/hello").query("name", "Sarah")))
     }
+
+    @Test
+    fun `if user visits hello endpoint with language specified, it responds Hello in their language`() {
+        assertEquals(Response(OK).body("Hello"), app(Request(Method.GET, "/en-US/hello")))
+        assertEquals(Response(OK).body("Bonjour"), app(Request(Method.GET, "/fr-FR/hello")))
+        assertEquals(Response(OK).body("G'day"), app(Request(Method.GET, "/en-AU/hello")))
+        assertEquals(Response(OK).body("Salve"), app(Request(Method.GET, "/it-IT/hello")))
+        assertEquals(Response(OK).body("Alright"), app(Request(Method.GET, "/en-GB/hello")))
+    }
+
+    @Test
+    fun `if user visits hello endpoint with language specified and name param, it responds Hello and name in their language`() {
+        assertEquals(Response(OK).body("Hello Sarah"), app(Request(Method.GET, "/en-US/hello").query("name", "Sarah")))
+        assertEquals(Response(OK).body("Bonjour Leah"), app(Request(Method.GET, "/fr-FR/hello").query("name", "Leah")))
+        assertEquals(Response(OK).body("G'day Ellie"), app(Request(Method.GET, "/en-AU/hello").query("name", "Ellie")))
+        assertEquals(Response(OK).body("Salve Alice"), app(Request(Method.GET, "/it-IT/hello").query("name", "Alice")))
+        assertEquals(Response(OK).body("Alright, Akash?"), app(Request(Method.GET, "/en-GB/hello").query("name", "Akash")))
+    }
 }
